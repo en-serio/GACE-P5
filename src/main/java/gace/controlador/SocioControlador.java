e0808d7648c23e9d69be81ae48d51d265cecdc82
Albert Recuero Campasol <albertrc7573@gmail.com>
2024-12-17 22:13:42 +0100

    Mis cambios

--- src/main/java/gace/controlador/SocioControlador.java
+++ src/main/java/gace/controlador/SocioControlador.java
@@ -121,8 +121,7 @@ public class SocioControlador {
         tipoSocioCombo.getItems().addAll("ESTÁNDAR", "FEDERADO", "INFANTIL");
 
         Dialog<String> dialog = new Dialog<>();
-        dialog.setHeight(400);
-        dialog.setWidth(500);
+        dialog.getDialogPane().setPrefSize(470, 350);
         dialog.setTitle("Registrar Socio");
         dialog.setHeaderText("Por favor ingrese los datos del socio:");
 
